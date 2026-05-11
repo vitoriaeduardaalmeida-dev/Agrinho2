9b32e0847823b35e07a634731857534dc556613d
Vitória Eduarda de Almeida <vitoria.eduarda.almeida@escola.pr.gov.br>
2026-05-11 09:25:49 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-document.addEventListener("DOMContentLoaded", function(){
+const temaBtn = document.getElementById("temaBtn");
 
-    const temaBtn = document.getElementById("temaBtn");
+temaBtn.onclick = function () {
 
-    temaBtn.addEventListener("click", function(){
+    document.body.classList.toggle("dark");
 
-        document.body.classList.toggle("dark");
+    if (document.body.classList.contains("dark")) {
 
-        if(document.body.classList.contains("dark")){
-            temaBtn.innerHTML = "☀️";
-        }else{
-            temaBtn.innerHTML = "🌙";
-        }
+        temaBtn.innerHTML = "☀️";
 
-    });
+    } else {
 
-});
+        temaBtn.innerHTML = "🌙";
+
+    }
+
+};
