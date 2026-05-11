d9618d2e8ac43a27f6d71ad902916a8a25e87403
Vitória Eduarda de Almeida <vitoria.eduarda.almeida@escola.pr.gov.br>
2026-05-11 09:24:45 -0300

    Refactor event listener formatting in script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function(){
 
     const temaBtn = document.getElementById("temaBtn");
 
-    temaBtn.addEventListener("click", function () {
+    temaBtn.addEventListener("click", function(){
 
         document.body.classList.toggle("dark");
 
         if(document.body.classList.contains("dark")){
             temaBtn.innerHTML = "☀️";
-        } else {
+        }else{
             temaBtn.innerHTML = "🌙";
         }
 
