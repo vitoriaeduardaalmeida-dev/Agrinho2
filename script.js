3645ef2521c2a23a0edc5ac29bb1ba70e56cb23a
Vitória Eduarda de Almeida <vitoria.eduarda.almeida@escola.pr.gov.br>
2026-05-18 10:24:15 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,3 @@
-console.log("JavaScript carregado com sucesso!");
-
-window.onload = function() {
-    const formulario = document.getElementById("meuFormulario");
-    const resposta = document.getElementById("resposta");
-
-    if (formulario) {
-        formulario.onsubmit = function(evento) {
-            evento.preventDefault(); // Impede a página de recarregar
-            
-            const nome = document.getElementById("nome").value;
-            
-            if (resposta) {
-                resposta.innerText = "Obrigado pelo contato, " + nome + "! Mensagem enviada.";
-                resposta.style.color = "green";
-                resposta.style.fontWeight = "bold";
-            }
-            
-            formulario.reset(); // Limpa o formulário
-        };
-    }
-};
+function modoEscuro() {
+  document.body.classList.toggle("dark");
+}
